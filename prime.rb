765b52786441cd9713c46560ebcb4d4c94f494c4
Ramsey Chapin <ramsey@ramseychapin.com>
2019-05-27 20:41:30 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,11 +4,12 @@
 def prime?(num)
   if num.abs == 0 || num.abs == 1
     return false
-  end
+  elsif
   (2..Math.sqrt(num.abs)).each { |i|
     if num % i == 0
       return false
     end
   }
+else
   true
 end
