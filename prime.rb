2531c481ecb5d08f5c2cbb3a9f491482f2b7c786
Ramsey Chapin <ramsey@ramseychapin.com>
2019-05-27 20:42:35 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+
+
+def prime?(num)
+  if num.abs == 0 || num.abs == 1
+    return false
+  end
+  
+  (2..Math.sqrt(num.abs)).each { |i|
+    if num % i == 0
+      return false
+    end
+  }
+  true
+end
